f4da9ca16d2c6dfdfbcfd46a59204c392ff7f648
yplulich <lucas.lichner@stud.hs-ruhrwest.de>
2026-04-10 00:35:50 +0200

    add procurement

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/procurement",
+        destination: "/procurement/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
